33f1ded1c80c784baf0b197de64091cd6ff6126b
KostaVesovic <kosta.vesovic@gmail.com>
2026-02-24 09:00:52 +0100

    Initial project setup, lint, pre-commit hook

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+})
